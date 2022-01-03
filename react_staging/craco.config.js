a36bd0cefe568482fbbbd4e5e2c11f482b1bcca9
reisen-pro <1054004534@qq.com>
2022-01-03 20:55:20 +0800

    react antd ui简单学习和自定义配置

---
+++ react_staging/craco.config.js
@@ -0,0 +1,18 @@
+// 自定义主题
+const CracoLessPlugin = require('craco-less');
+
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            modifyVars: { '@primary-color': '#1DA57A' },
+            javascriptEnabled: true,
+          },
+        },
+      },
+    },
+  ],
+};
--- react_staging/src/index.js
+++ react_staging/src/index.js
@@ -4,7 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // 引入App
 import App from './App';
-import 'antd/dist/antd.css'
+import './App.less';
 
 // 渲染
 ReactDOM.render(<App/>, document.getElementById('root')
